512cd0c648828a17c1d74699beb02d2b363e7973
Younes-ch <younes.jmell10@gmail.com>
2024-01-15 16:32:33 +0100

    Display fetched images with the loading state.

--- src/components/GalleryGrid.tsx
+++ src/components/GalleryGrid.tsx
@@ -1,9 +1,32 @@
+import useImages from "@/hooks/useImages";
+import ImageCard from "./ImageCard";
+import { Skeleton } from "./ui/skeleton";
+
+const SkeletonGrid = () => {
+  const skeletons = new Array(16).fill(1, 1, 16);
+
+  return skeletons.map((skeleton, index) => (
+    <div className="flex flex-col gap-2">
+      <Skeleton key={index} className="w-[300px] h-[150px] rounded-sm" />
+      <Skeleton key={index} className="w-[300px] h-[10px] rounded-md" />
+      <Skeleton key={index} className="w-[300px] h-[10px] rounded-md" />
+    </div>
+  ));
+};
 
 const GalleryGrid = () => {
+  const { data, error, isLoading } = useImages();
+
+  if (error) throw error;
+
   return (
     <div className="container mx-auto px-4">
-      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4">
-
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        {isLoading ? (
+          <SkeletonGrid />
+        ) : (
+          data?.hits.map((image) => <ImageCard key={image.id} image={image} />)
+        )}
       </div>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ThemeProvider } from "./components/theme-provider.tsx";
 import "./index.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
