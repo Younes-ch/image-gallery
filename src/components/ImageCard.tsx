2542c38c71eb4bc3819386b69c0cbab98afe4219
Younes-ch <younes.jmell10@gmail.com>
2024-01-15 16:30:13 +0100

    Redesign image card.

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,45 +1,59 @@
-import { Eye, DownloadIcon, Heart } from "lucide-react"
+import Image from "@/entities/Image";
+import { Eye, DownloadIcon, Heart } from "lucide-react";
+import { Badge } from "./ui/badge";
+import { HoverCard, HoverCardTrigger, HoverCardContent } from "./ui/hover-card";
 
-const ImageCard = () => {
-  const tags = ["dog", "cat", "rabbit"];
+interface Props {
+  image: Image;
+}
+
+const ImageCard = ({ image }: Props) => {
+  const tags = image.tags.split(", ");
 
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-2xl border border-primary-foreground">
-      <img
-        src="https://pixabay.com/get/g57ebf94375a43a9b856f6d811f2972d388658a24c5080dae2d8f5614bc134abf46f51687cbe2d138c93f0af73af390bc_640.jpg"
-        alt=""
-        className="w-full"
-      />
-      <div className="px-6 py-4">
-        <h3 className="text-primary scroll-m-20 text-2xl font-semibold tracking-tight">
-          The Joke Tax
-        </h3>
-        <ul className="flex flex-col gap-1">
-          <li className="flex gap-1">
-            <Eye />
-            1013574
-          </li>
-          <li className="flex gap-1">
-            <DownloadIcon />
-            769513
-          </li>
-          <li className="flex gap-1">
-            <Heart />
-            1876
-          </li>
-        </ul>
-      </div>
-      <div className="px-6 py-4">
-        {tags.map((tag, index) => (
-          <span
-            key={index}
-            className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
-          >
-            #{tag}
-          </span>
-        ))}
-      </div>
-    </div>
+    <HoverCard>
+      <HoverCardTrigger>
+        <div className="max-w-sm h-[450px] rounded overflow-hidden shadow-2xl border border-primary-foreground hover:scale-105 hover:transition-transform">
+          <img
+            src={image.webformatURL}
+            alt=""
+            className="w-full h-[250px] object-cover"
+          />
+          <div className="px-6 py-4 flex flex-col justify-between gap-5">
+            <div>
+              <div className="flex gap-2">
+                <img src={image.userImageURL} className="rounded-full w-10 h-10" />
+                <h3 className="text-primary scroll-m-20 text-2xl font-semibold tracking-tight mb-5">
+                  By {image.user}
+                </h3>
+              </div>
+              <ul className="flex justify-evenly">
+                <li className="flex gap-1">
+                  <Eye />
+                  {image.views}
+                </li>
+                <li className="flex gap-1">
+                  <DownloadIcon />
+                  {image.downloads}
+                </li>
+                <li className="flex gap-1">
+                  <Heart className="text-red-400" />
+                  {image.likes}
+                </li>
+              </ul>
+            </div>
+            <div className="flex gap-3 flex-wrap justify-center">
+              {tags.map((tag, index) => (
+                <Badge key={index}>{tag}</Badge>
+              ))}
+            </div>
+          </div>
+        </div>
+      </HoverCardTrigger>
+      <HoverCardContent side="left" sideOffset={-70}>
+        <img src={image.webformatURL} />
+      </HoverCardContent>
+    </HoverCard>
   );
 };
 
