53889b6c607adc24d27fe5e048e19e137058adc7
Younes-ch <younes.jmell10@gmail.com>
2024-01-15 13:51:50 +0100

    Build responsive layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import NavBar from "@/components/ui/navbar";
+import GalleryGrid from "./components/GalleryGrid";
 
 function App() {
   return (
     <>
       <NavBar />
+      <GalleryGrid />
     </>
   );
 }
---
+++ src/components/GalleryGrid.tsx
@@ -0,0 +1,12 @@
+
+const GalleryGrid = () => {
+  return (
+    <div className="container mx-auto px-4">
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4">
+
+      </div>
+    </div>
+  );
+};
+
+export default GalleryGrid;
