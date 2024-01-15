b91fa30ee4ade9feee18faba1d47091e38ac61bd
Younes-ch <younes.jmell10@gmail.com>
2024-01-15 16:27:02 +0100

    Add Image entity.

---
+++ src/entities/Image.ts
@@ -0,0 +1,10 @@
+export default interface Image {
+    id: number;
+    tags: string;
+    webformatURL: string;
+    views: number;
+    downloads: number;
+    likes: number;
+    user: string;
+    userImageURL: string;
+}
