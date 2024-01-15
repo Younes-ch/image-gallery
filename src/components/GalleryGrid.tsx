0ebacdbf3f49c316ed9ded31bb703413956d6d8f
Younes-ch <younes.jmell10@gmail.com>
2024-01-15 20:31:10 +0100

    Implementing infine scrolling.

--- src/components/GalleryGrid.tsx
+++ src/components/GalleryGrid.tsx
@@ -2,22 +2,31 @@ import useImages from "@/hooks/useImages";
 import ImageCard from "./ImageCard";
 import { Skeleton } from "./ui/skeleton";
 import imageQueryStore from "@/stores/imageQueryStore";
+import React from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const SkeletonGrid = () => {
   const skeletons = new Array(16).fill(1);
 
-  return skeletons.map((_, index) => (
-    <div key={index} className="flex flex-col gap-2">
-      <Skeleton className="w-[300px] h-[150px] rounded-sm" />
-      <Skeleton className="w-[300px] h-[10px] rounded-md" />
-      <Skeleton className="w-[300px] h-[10px] rounded-md" />
+  return (
+    <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+      {skeletons.map((_, index) => (
+        <div key={index} className="flex flex-col gap-2">
+          <Skeleton className="w-[300px] h-[150px] rounded-sm" />
+          <Skeleton className="w-[300px] h-[10px] rounded-md" />
+          <Skeleton className="w-[300px] h-[10px] rounded-md" />
+        </div>
+      ))}
     </div>
-  ));
+  );
 };
 
 const GalleryGrid = () => {
   const imageQuery = imageQueryStore((store) => store.imageQuery);
-  const { data, error, isLoading } = useImages(imageQuery);
+  const { data, error, fetchNextPage, hasNextPage } = useImages(imageQuery);
+
+  const fetchedImagesCount =
+    data?.pages.reduce((total, page) => total + page.hits.length, 0) || 0;
 
   return (
     <div className="container mx-auto px-4">
@@ -26,15 +35,22 @@ const GalleryGrid = () => {
           An error has occured.
         </h1>
       ) : (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-          {isLoading ? (
-            <SkeletonGrid />
-          ) : (
-            data?.hits.map((image) => (
-              <ImageCard key={image.id} image={image} />
-            ))
-          )}
-        </div>
+        <InfiniteScroll
+          dataLength={fetchedImagesCount}
+          next={() => fetchNextPage()}
+          hasMore={!!hasNextPage}
+          loader={<SkeletonGrid />}
+        >
+          <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+            {data?.pages.map((page, index) => (
+              <React.Fragment key={index}>
+                {page.hits.map((image) => (
+                  <ImageCard key={image.id} image={image} />
+                ))}
+              </React.Fragment>
+            ))}
+          </div>
+        </InfiniteScroll>
       )}
     </div>
   );
--- src/hooks/useImages.ts
+++ src/hooks/useImages.ts
@@ -1,20 +1,26 @@
 import Image from "@/entities/Image";
 import APIClient from "@/services/api-client";
 import { ImageQuery } from "@/stores/imageQueryStore";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import ms from "ms";
 
 const apiClient = new APIClient<Image>();
 
-const useImages = (imageQuery: ImageQuery) => useQuery({
+const useImages = (imageQuery: ImageQuery) => useInfiniteQuery({
     queryKey: ["images", imageQuery],
-    queryFn: () => apiClient.getAll({
+    queryFn: ({ pageParam = 1 }) => apiClient.getAll({
         params: {
             q: imageQuery.query,
             image_type: imageQuery.imageType,
             order: imageQuery.sortOrder,
-            safesearch: true
+            safesearch: true,
+            page: pageParam
         }
     }),
+    getNextPageParam: (lastPage, allPages) => {
+        return lastPage.hits.length >= 1 ? allPages.length + 1 : undefined;
+    },
+    staleTime: ms('24h')
 });
 
 export default useImages;
